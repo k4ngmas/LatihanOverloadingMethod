eb1d2700e8ae88bc56f1d6b2a471da322c9a9708
k4ngmas <68710943+k4ngmas@users.noreply.github.com>
2021-04-01 00:01:34 +0700

    Add project files.

---
+++ Numbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classmemberpart2
+{
+    public class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            return number1 - number2 - number2;
+        }
+
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            return (int)number3 + number1 - number1 - number2;
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            return number1 + number2 - number2;
+        }
+
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            return number2 + (int)number3 - number1 + number1 - number2;
+        }
+
+        
+    }
+}
